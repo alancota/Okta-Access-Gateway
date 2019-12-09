db415284e4f4aa8ff281b8456365374a317ca430
indranilokg <58342808+indranilokg@users.noreply.github.com>
2019-12-09 17:52:53 -0600

    Added default egress rule

--- deploy/aws/oag-config/ec2.tf
+++ deploy/aws/oag-config/ec2.tf
@@ -25,6 +25,20 @@ resource "aws_security_group" "v_oag_security_group" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "v_oag_instance" {
