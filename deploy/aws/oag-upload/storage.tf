ec5b299beb966e5bd4222ada4c1aac26f2372f94
indranilokg <58342808+indranilokg@users.noreply.github.com>
2019-12-18 16:42:07 -0600

    minor changes

--- deploy/aws/oag-upload/storage.tf
+++ deploy/aws/oag-upload/storage.tf
@@ -11,5 +11,5 @@ resource "aws_s3_bucket_object" "v_oag_ova_object" {
   bucket = aws_s3_bucket.v_oag_ova_bucket.id
   key    = var.S3_OBJECT_KEY
   source = var.OAG_OVA_FILE_PATH
-  etag = filemd5("Okta-Access-Gateway.ova")
+  etag = filemd5(var.OAG_OVA_FILE_PATH)
 }
